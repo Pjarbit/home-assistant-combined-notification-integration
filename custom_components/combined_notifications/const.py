deb3973f6e01efd9da62cb00eee6e01881339949
Pjarbit <131613255+Pjarbit@users.noreply.github.com>
2025-05-07 13:50:03 -0400

    Update const.py

--- custom_components/combined_notifications/const.py
+++ custom_components/combined_notifications/const.py
@@ -1,32 +1,37 @@
 """Constants for the Combined Notifications integration."""
-
 DOMAIN = "combined_notifications"
-NOTIFICATION_TEMPLATES_PATH = "notification_templates"
 
-# Color options for the notification card
 COLORS = [
-    "Use YOUR Current Theme Color", "Red", "Green", "Blue", "Yellow", "Orange",
-    "Purple", "Gray", "White", "Black", "Teal", "Transparent Background"
+    "Use YOUR Current Theme Color",
+    "Red",
+    "Green",
+    "Blue",
+    "Yellow",
+    "Orange",
+    "Purple",
+    "Gray",
+    "White",
+    "Black",
+    "Teal",
+    "Transparent Background",
 ]
 
-# Operators with friendly names
 OPERATORS = [
-    "equals (==)",
-    "not equals (!=)",
-    "greater than (>)",
-    "less than (<)"
+    "==",
+    "!=",
+    ">",
+    "<",
 ]
 
-# Map friendly operator names to symbols
 OPERATOR_MAP = {
-    "equals (==)": "==",
-    "not equals (!=)": "!=",
-    "greater than (>)": ">",
-    "less than (<)": "<"
+    "==": "equals",
+    "!=": "not equals",
+    ">": "greater than",
+    "<": "less than",
 }
 
-# Color mapping for RGB values (or CSS color keywords)
 COLOR_MAP = {
+    "Use YOUR Current Theme Color": "",
     "Red": "rgb(190, 11, 11)",
     "Green": "rgb(19, 161, 14)",
     "Blue": "rgb(2, 136, 209)",
@@ -37,5 +42,5 @@ COLOR_MAP = {
     "White": "rgb(255, 255, 255)",
     "Black": "rgb(0, 0, 0)",
     "Teal": "rgb(0, 128, 128)",
-    "Transparent Background": "transparent"
+    "Transparent Background": "transparent",
 }
