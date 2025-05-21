f3b3dbeef2633115d8766bec148d73f6bbdbb13b
Pjarbit <131613255+Pjarbit@users.noreply.github.com>
2025-05-21 08:39:23 -0400

    Update __init__.py

--- custom_components/combined_notifications/__init__.py
+++ custom_components/combined_notifications/__init__.py
@@ -1,6 +1,10 @@
-"""The Combined Notifications integration."""
+"""Combined Notifications integration."""
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Combined Notifications from a config entry."""
@@ -9,4 +13,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, ["sensor"]):
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+    return unload_ok
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload the config entry."""
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
