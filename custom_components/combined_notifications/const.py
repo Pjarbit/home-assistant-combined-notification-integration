1c86d101f3dd713778516efd19b17dfa8e7d33df
Pjarbit <131613255+Pjarbit@users.noreply.github.com>
2025-05-21 09:34:58 -0400

    Update const.py

--- custom_components/combined_notifications/const.py
+++ custom_components/combined_notifications/const.py
@@ -1,41 +1,34 @@
 """Constants for the Combined Notifications integration."""
+
 DOMAIN = "combined_notifications"
+NOTIFICATION_TEMPLATES_PATH = "notification_templates"
 
+# Color options for the notification card
 COLORS = [
-    "Use YOUR Current Theme Color",
-    "Red",
-    "Green",
-    "Bright Green",
-    "Blue",
-    "Yellow",
-    "Orange",
-    "Purple",
-    "Gray",
-    "White",
-    "Black",
-    "Teal",
-    "Transparent Background",
+    "Use YOUR Current Theme Color", "Red", "Green", "Blue", "Yellow", "Orange",
+    "Purple", "Gray", "White", "Black", "Teal", "Transparent Background"
 ]
 
+# Operators with friendly names
 OPERATORS = [
-    "==",
-    "!=",
-    ">",
-    "<",
+    "equals (==)",
+    "not equals (!=)",
+    "greater than (>)",
+    "less than (<)"
 ]
 
+# Map friendly operator names to symbols
 OPERATOR_MAP = {
-    "==": "equals",
-    "!=": "not equals",
-    ">": "greater than",
-    "<": "less than",
+    "equals (==)": "==",
+    "not equals (!=)": "!=",
+    "greater than (>)": ">",
+    "less than (<)": "<"
 }
 
+# Color mapping for RGB values (or CSS color keywords)
 COLOR_MAP = {
-    "Use YOUR Current Theme Color": "",
     "Red": "rgb(190, 11, 11)",
-    "Green": "rgb(38,141,53)",
-    "Bright Green": "rgb(47, 207, 118)",  # Fixed semicolon to colon and added closing quote
+    "Green": "rgb(19, 161, 14)",
     "Blue": "rgb(2, 136, 209)",
     "Yellow": "rgb(255, 215, 0)",
     "Orange": "rgb(255, 140, 0)",
@@ -45,4 +38,5 @@ COLOR_MAP = {
     "Black": "rgb(0, 0, 0)",
     "Teal": "rgb(0, 128, 128)",
     "Transparent Background": "transparent",
+    "Use YOUR Current Theme Color": "var(--primary-background-color)"  # CSS variable for theme background
 }
