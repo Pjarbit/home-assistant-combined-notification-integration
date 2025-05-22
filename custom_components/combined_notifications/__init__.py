661c3cc9012bf867fbb23871369f3984717a64b3
Pjarbit <131613255+Pjarbit@users.noreply.github.com>
2025-05-22 12:36:31 -0400

    Update __init__.py

--- custom_components/combined_notifications/__init__.py
+++ custom_components/combined_notifications/__init__.py
@@ -8,6 +8,7 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Combined Notifications from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
