71f9e4830d242182b3dcddc68ebf4ac5c2d3b0f1
Philip J. Arbit <131613255+Pjarbit@users.noreply.github.com>
2026-04-29 08:37:07 -0400

    Update version number in combined_notifications_panel.js

--- custom_components/combined_notifications/combined_notifications_panel.js
+++ custom_components/combined_notifications/combined_notifications_panel.js
@@ -1,5 +1,5 @@
 /**
- * Combined Notifications Panel v5.8.0
+ * Combined Notifications Panel v5.0.8
  * Custom Lovelace panel for configuring Combined Notifications sensors.
  * Communicates with HA via websocket API.
  */
