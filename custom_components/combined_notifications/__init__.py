4aaf8feadf76d93cf5ae8997d02b1f1625f582ae
Philip J. Arbit <131613255+Pjarbit@users.noreply.github.com>
2026-05-07 18:36:27 -0400

    Update integration version to 5.5.9 and version slug

--- custom_components/combined_notifications/__init__.py
+++ custom_components/combined_notifications/__init__.py
@@ -1,5 +1,5 @@
 """Combined Notifications integration."""
-# Integration version: 5.5.5
+# Integration version: 5.5.9
 import logging
 import os
 from homeassistant.config_entries import ConfigEntry
@@ -11,7 +11,7 @@ from .const import DOMAIN, COLOR_MAP
 
 _LOGGER = logging.getLogger(__name__)
 
-VERSION_SLUG = "554"
+VERSION_SLUG = "559"
 PANEL_URL = f"/combined_notifications_panel_{VERSION_SLUG}"
 PANEL_FILENAME = "combined_notifications_panel.js"
 
