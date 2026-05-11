26e62a010c2b2492ac2338047181041bac23d01c
Philip J. Arbit <131613255+Pjarbit@users.noreply.github.com>
2026-05-11 09:52:00 -0400

    Update integration version to 5.6.4

--- custom_components/combined_notifications/__init__.py
+++ custom_components/combined_notifications/__init__.py
@@ -1,5 +1,5 @@
 """Combined Notifications integration."""
-# Integration version: 5.6.2
+# Integration version: 5.6.4
 import logging
 import os
 from homeassistant.config_entries import ConfigEntry
